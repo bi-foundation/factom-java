ee5437897dd922f1d452c113bebf719348e97892
nklomp <nklomp@sphereon.com>
2018-08-19 02:57:46 +0200

    SPMS-239 Updated copyrights

--- src/test/java/org/blockchain_innovation/factom/client/ChainEntryIT.java
+++ src/test/java/org/blockchain_innovation/factom/client/ChainEntryIT.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Blockchain Innovation Foundation <https://blockchain-innovation.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.blockchain_innovation.factom.client;
 
 import org.blockchain_innovation.factom.client.data.FactomException;
@@ -21,7 +37,7 @@ import java.util.List;
 import java.util.Random;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class ChainEntryTestIT extends AbstractClientTest {
+public class ChainEntryIT extends AbstractClientTest {
 
     public static final int SLEEP_TIME = 5000;
     private static String chainId /*= "23fc40b5d301f8c40513cb1363439bc23e6c21856073abefdb1a2a2e49baba3b"*/;
